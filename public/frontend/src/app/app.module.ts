ce09d9ecc1fa0417500b598c2e46dbd87de0c1db
Darwin <darwinpeacehope@gmail.com>
2018-04-24 22:26:45 -0500

    tipo identificacion init

--- public/frontend/src/app/app.module.ts
+++ public/frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NomIdentifytypeComponent } from './nomenclature/nom-identifytype/nom-identifytype.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NomIdentifytypeComponent
   ],
   imports: [
     BrowserModule
