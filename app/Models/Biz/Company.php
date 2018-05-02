4e515bbb796e8fcbb9849e17397be4ef2d67cd77
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2018-05-01 19:29:39 -0500

    Role CRUD Module

---
+++ app/Models/Biz/Company.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Biz;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+
+    protected $table = 'biz_company';
+
+    protected $primaryKey = 'idcompany';
+
+}
