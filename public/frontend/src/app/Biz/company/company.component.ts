c15c13a0a458ed5236d8c1b02aedbdad16efde1d
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2018-05-04 19:57:50 -0500

    Company Module

--- public/frontend/src/app/Biz/company/company.component.ts
+++ public/frontend/src/app/Biz/company/company.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { BcompanyService } from './../../service/bcompany/bcompany.service';
+declare var jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-company',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company.component.css']
 })
 export class CompanyComponent implements OnInit {
-
-  constructor() { }
+  @Input() tem_edit_role: any;
+  constructor(private company: BcompanyService) { }
 
   ngOnInit() {
+
+    this.tem_edit_role = {
+
+      businessname: '',
+      tradename: '',
+      identify: '',
+      phone: '',
+      address: '',
+      email: '',
+      urlweb: ''
+
+    };
+
+  }
+
+  update(data: any) {
+    /*this.role.update(data.idrole, data).subscribe(
+      (response) => {
+        if (response.success === true) {
+          $('#mdlUpdate').modal('hide');
+          this.update_component_father.emit(true);
+        } else {
+          $('#mdlUpdate').modal('hide');
+          this.update_component_father.emit(false);
+        }
+      },
+      (error) => {
+        console.log('POST call in error", respons', error);
+        $('#mdlUpdate').modal('hide');
+        this.update_component_father.emit(false);
+      });*/
   }
 
 }
