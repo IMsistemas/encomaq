d22237d7146f9cac88715e78ccf9bf2dabaec233
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2018-05-08 09:38:04 -0500

    Compile Project

--- public/frontend/src/app/system/user/updateuser/updateuser.component.ts
+++ public/frontend/src/app/system/user/updateuser/updateuser.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { UserService } from './../../../service/user/user.service';
-import { RoleService } from './../../../service/role/role.service';
 
 declare var jquery: any;
 declare var $: any;
@@ -11,21 +10,16 @@ declare var $: any;
   templateUrl: './updateuser.component.html',
   styleUrls: ['./updateuser.component.css']
 })
-export class UpdateuserComponent implements OnInit {
-  @Input() tem_edit_user: any;
+export class UpdateuserComponent implements OnInit {  
   @Input() listRole: Observable<any>;
+  @Input() tem_edit_user: any;
   @Output() update_component_father = new EventEmitter<boolean>();
-  // listRole: Observable<any>;
-  constructor(private user: UserService, private role: RoleService) { }
+  constructor(private user: UserService) { }
 
-  ngOnInit() {
-    // this.getListRole();
+  ngOnInit() {    
   }
 
-  /* getListRole() {
-    this.listRole = this.role.getActiveRole();
-  }*/
-
+  
   update(data: any) {
     this.user.update(data.iduser, data).subscribe(
       (response) => {
