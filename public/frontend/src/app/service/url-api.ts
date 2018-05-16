3075262779c76c8ed556971aa655be2ea28ba5be
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2018-05-16 10:56:58 -0500

    Compile Project

--- public/frontend/src/app/service/url-api.ts
+++ public/frontend/src/app/service/url-api.ts
@@ -1,6 +1,7 @@
 export class UrlApi {
     public url: String;
     constructor() {
+        // this.url = 'http://encomaq.ditatechnology.com/';
         this.url = 'http://localhost:8000/';
     }
     get_url_api(): String {
