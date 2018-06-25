d7ce2b5dd9d154cd2d393fbbbc46fe65c4b827db
Darwin <darwinpeacehope@gmail.com>
2018-06-24 22:44:37 -0500

    BUILD

--- public/frontend/src/app/service/url-api.ts
+++ public/frontend/src/app/service/url-api.ts
@@ -2,7 +2,7 @@ export class UrlApi {
     public url: String;
     constructor() {
         // this.url = 'http://encomaq.ditatechnology.com/';
-        this.url = 'http://localhost:8000/';
+         this.url = 'http://localhost:8000/';
     }
     get_url_api(): String {
         return this.url;
