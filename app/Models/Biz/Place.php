fbd7db2afe9f70538f023eb0468b8a1a7c0fdecc
Raidel Berrillo <raidelbg84@gmail.com>
2018-08-13 08:53:03 -0500

    Place CRUD Module

---
+++ app/Models/Biz/Place.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Biz;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Place extends Model
+{
+    protected $table = 'biz_place';
+
+    protected $primaryKey = 'idplace';
+}
---
+++ app/Models/Biz/ReferralGuidePlace.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Biz;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReferralGuidePlace extends Model
+{
+    protected $table = 'biz_referralguide_place';
+
+    protected $primaryKey = null;
+    public $incrementing = false;
+    public $timestamps = false;
+}
