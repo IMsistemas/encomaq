f88e6d7e58c9902f6f1b48923d2c51807d7f1998
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2019-07-21 03:16:32 -0500

    Role CRUD Module

--- frontend/src/app/service/url-api.ts
+++ frontend/src/app/service/url-api.ts
@@ -2,7 +2,8 @@ export class UrlApi {
     public url: String;
     constructor() {
         // this.url = 'http://backend-encomaq.encomaq.es/';
-        this.url = 'http://localhost:8000/';
+        this.url = 'http://back_encomaq.apps-developing.com/';
+        // this.url = 'http://localhost:8000/';
     }
     get_url_api(): String {
         return this.url;
